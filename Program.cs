8ebce393c0c735c2055c44e01dd12e5701c95f0b
Gabriela Dourado <gdourado.066@gmail.com>
2024-06-06 12:52:40 -0300

    Manipulating archives TXT!
    Basic practical exercise carried out with the following requirements:
    
    EXERCISE 2 -
    
    REQ 1 - The input will not have fixed assignments and vector parameters, that is, we do not have the order of variables as was the case in args[1], args[2], args[3]...
    
    REQ 2 - The input must not accept empty parameters.
    
    Example: -inputfile <filename> -outputfile <filename> -oldword -newword <word> -count -insensitive/sensitive
    
    -newword must not be without parameters. It should not be accepted in this way.
    
    REQ 3 - The input must receive whether it wants to be read in a sensitive or insensitive way, if it is not sent in the command, a specific type must be considered in a default.
    
    REQ 4 - The count must count how many times the word OldWord is in the file.
    
    REQ 5 - In cases where we receive count, we will not change the words. The new word parameter will not matter if it is not in the file, EXCEPT if it is entered incorrectly/incompletely.
    
    REQ 6 - If it doesn't come, replace the words.
    
    -------------------------------------------------- -------------------------------------------------- ---------------------------
    
    COMMANDS:
    
    wordpreplace -inputfile <filename> -outputfile <filename> -oldword <word> -newword <word> -count -insensitive/sensitive
    
    Example 1:
    wordpreplace -inputfile original.txt -outputfile saida.txt -oldword home -newword work
    
    Example 2:
    wordpreplace -outputfile saida.txt -oldword home -newword work -inputfile original.txt
    
    Wrong Example 1:
    wordpreplace -outputfile saida.txt -oldword -newword work -inputfile original.txt
    Example 3:
    wordpreplace -outputfile saida.txt -oldword home -newword kdkjfgjk -inputfile original.txt -count -insensitive

---
+++ Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace WordReplaceThursday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? inputfile = null;
+            string? outputfile = null;
+            string? oldword = null;
+            string? newword = null;
+
+            bool count = false;
+            bool caseInsensitive = true;
+
+            //loops through the arguments given on the command line
+            for (int i = 0; i < args.Length - 1; i++)
+            {
+                switch (args[i])
+                {
+                    //fill in the input variables
+                    case "-inputfile":
+                        if (i < args.Length) inputfile = args[++i];
+                        //i += 1;
+                        break;
+
+                    case "-outputfile":
+                        if (i < args.Length) outputfile = args[++i];
+                        break;
+
+                    case "-oldword":
+                        if (i < args.Length) oldword = args[++i];
+                        break;
+
+                    case "-newword":
+                        if (i < args.Length) newword = args[++i];
+                        break;
+
+                    case "-count":
+                        count = true;
+                        break;
+
+                    case "-insensitive":
+                        caseInsensitive = true;
+                        break;
+
+                    case "-sensitive":
+                        caseInsensitive = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Argumento invalido: " + args[i]);
+                        return;
+
+                }
+            }
+
+            //Mandatory arguments
+            //IsNullOrEmpty - This is the function that observes whether it is null and whether it has at least 1 character
+            if (string.IsNullOrEmpty(inputfile) || string.IsNullOrEmpty(outputfile) || string.IsNullOrEmpty(oldword))
+            {
+                Console.WriteLine("Ausencia de comandos obrigatorios de entrada.");
+                return;
+            }
+
+            //Read the file
+            string reader = File.ReadAllText(inputfile);
+
+            //Sensitivity treatment - sensitive option will be the default
+            if (caseInsensitive)
+            {
+                reader = reader.ToLower();
+                oldword = oldword.ToLower();
+            }
+
+            //For word count 
+            int wordcount = CountOcurrences(reader, oldword);
+            Console.WriteLine($"A palavra {oldword} ocorre {wordcount} vezes");
+
+            //So that you don't make substitutions
+            if (count)
+            {
+                return;
+            }
+
+                //validation of the new word 
+                if (string.IsNullOrEmpty(newword))
+            {
+                Console.WriteLine("Ausencia da nova palavra!");
+                return;
+            }
+
+            //replacement 
+            reader = ReplaceWord(reader, oldword, newword, caseInsensitive);
+
+            //output writing
+            File.WriteAllText(outputfile, reader);
+            Console.WriteLine("Arquivo gravado!");
+
+
+            ///////FUNCTIONS 
+
+            static int CountOcurrences(string reader, string word)
+            {
+                int count = 0; //specified: int
+                int index = 0;
+
+                while ((index = reader.IndexOf(word, index)) != -1)
+                {
+                    count++;
+                    index += word.Length; //update to next character
+                }
+                return count;
+            }
+
+            static string ReplaceWord(string reader, string oldword, string newword, bool caseInsensitive) //attention for the type the caseInsensitive
+            {
+                if (caseInsensitive) return ReplaceCaseInsensitive(reader, oldword, newword);
+                return reader.Replace(oldword, newword);
+            }
+
+            //optimizing with StringBuilder
+            static string ReplaceCaseInsensitive(string reader, string oldword, string newword)
+            {
+                System.Text.StringBuilder result = new System.Text.StringBuilder();
+                int prevIndex = 0;
+                int index = 0;
+                while ((index = reader.IndexOf(oldword, prevIndex)) != -1)
+                {
+                    result.Append(reader.Substring(prevIndex, index - prevIndex)).Append(newword);
+                    prevIndex = index + oldword.Length;
+                }
+                result.Append(reader.Substring(prevIndex));
+                return result.ToString();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
